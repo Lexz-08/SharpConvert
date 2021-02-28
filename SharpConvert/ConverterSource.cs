f06447fcbf96b338bda4a599eb70909931c24c04
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-02-27 20:44:39 -0800

    Add project files.

---
+++ SharpConvert/ConverterSource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace SharpConvert
+{
+	public static class Extension
+    {
+		public static T ConvertTo<T>(this object obj)
+		{
+			TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+			return (T)converter.ConvertFrom(obj);
+		}
+	}
+}
