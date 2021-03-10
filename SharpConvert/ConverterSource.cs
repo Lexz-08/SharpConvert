c67fac52ca88be1514069cda3c6353659d3bbaf5
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-03-09 20:16:14 -0800

    added more conversions

--- SharpConvert/ConverterSource.cs
+++ SharpConvert/ConverterSource.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Drawing;
 
 namespace SharpConvert
 {
@@ -8,6 +9,47 @@ namespace SharpConvert
 		{
 			TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
 			return (T)converter.ConvertFrom(obj);
+		}
+		public static T FromString<T>(this T obj, string str)
+		{
+			TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+			return (T)converter.ConvertFromString(str);
+		}
+	}
+
+	public class Converter
+	{
+		public static Size PointToSize(Point pt)
+		{
+			return new Size(pt.X, pt.Y);
+		}
+		public static Point SizeToPoint(Size sz)
+		{
+			return new Point(sz.Width, sz.Height);
+		}
+
+		public static SizeF PointFToSizeF(PointF pt)
+		{
+			return new SizeF(pt.X, pt.Y);
+		}
+		public static PointF SizeFToPointF(SizeF sz)
+		{
+			return new PointF(sz.Width, sz.Height);
+		}
+
+		public static SizeF PointToSizeF(Point pt)
+		{
+			return new SizeF(pt.X, pt.Y);
+		}
+		public static PointF SizeToPointF(Size sz)
+		{
+			return new PointF(sz.Width, sz.Height);
+		}
+
+		public static T EnumFromString<T>(T enumeration, string str)
+		{
+			TypeConverter enumConverter = TypeDescriptor.GetConverter(typeof(T));
+			return (T)enumConverter.ConvertFromString(str);
 		}
 	}
 }
